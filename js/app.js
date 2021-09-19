1f855fe9cc880e301706c709bbedd4d52b6edf92
RafaelMenchaca <arafael.menchaca@gmail.com>
2021-09-18 19:58:48 -0500

    improved js logic

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+let userScore     = 0;
+let computerScore = 0;
+const userScore_span     = document.getElementById('user-score');
+const computerScore_span = document.getElementById('computer-score');
+const scoreBoard_div     = document.getElementById('.score-board');
 const userChoiceDisplay  = document.getElementById('user-choice');
 const compuChoiceDisplay = document.getElementById('computer-choice');
 const resultDisplay      = document.getElementById('resultado');
@@ -11,6 +16,7 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
     userChoiceDisplay.innerHTML = userChoice;
     generateComputerChoice()
     getResult()
+    gameOver(userScore, computerScore);
 }));
 
 function generateComputerChoice() {
@@ -28,6 +34,16 @@ function generateComputerChoice() {
     compuChoiceDisplay.innerHTML = computerChoice;
 }
 
+function winUser() {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+}
+
+function winComputer() {
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+}
+
 // rock beat scissors, scissors beat paper , paper beat rock 
 function getResult(){
     if (computerChoice === userChoice){
@@ -35,21 +51,44 @@ function getResult(){
     }
     if (computerChoice === 'rock' && userChoice === 'paper'){
         result = 'Paper beat Rock, You Win!'
+        winUser()
     }
     if (computerChoice === 'rock' && userChoice === 'scissors'){
         result = 'Rock beat Scissors, You Lose!'
+        winComputer()
     }
     if (computerChoice === 'paper' && userChoice === 'scissors'){
         result = 'Scissors beat Paper, You Win!'
+        winUser()
     }
     if (computerChoice === 'paper' && userChoice === 'rock'){
         result = 'Paper beat Rock, You Lose!'
+        winComputer()
     }
     if (computerChoice === 'scissors' && userChoice === 'rock'){
         result = 'Rock beat Scissors, You Win!'
+        winUser()
     }
     if (computerChoice === 'scissors' && userChoice === 'paper'){
         result = 'Scissors beat paper, You Lose!'
+        winComputer()
     }
     resultDisplay.innerHTML = result;
 }
+
+function gameOver(user, compu){
+    if(user === 5 && user > compu){
+        alert("Game Over You Win!");
+        userScore = 0;
+        computerScore = 0;
+        userScore_span.innerHTML = 0;
+        computerScore_span.innerHTML = 0;
+    } else if ( compu === 5 && compu > user){
+        alert("Game Over You Lose!");
+        userScore = 0;
+        computerScore = 0;
+        userScore_span.innerHTML = 0;
+        computerScore_span.innerHTML = 0;
+    }
+}
+
